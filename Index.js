d3c813614b542df5e2ee58508d09746e71b048d2
Mithun Pradeep <mithunpradeep448@gmail.com>
2018-09-27 18:15:55 +0530

    Hello World

--- Index.js
+++ Index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/',(request,response)=>{
-    response.send('<!Doctype html><head>Mithun</head></html>');
+    response.send('Hello World!');
 })
 
 app.listen(3000,(response)=>{
